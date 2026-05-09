8a7485cbe9e830f361c924b577bcc46acb211a1c
AlenaK13 <kruglovatm@mail.ru>
2026-05-09 15:13:42 +0300

    fix clang

--- mlir/compiler-course/kruglova_a_lab4/callcountpass_kruglova.cpp
+++ mlir/compiler-course/kruglova_a_lab4/callcountpass_kruglova.cpp
@@ -1,43 +1,46 @@
-#include "mlir/Dialect/LLVMIR/LLVMDialect.h"
 #include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/Dialect/LLVMIR/LLVMDialect.h"
 #include "mlir/IR/Builders.h"
 #include "mlir/IR/PatternMatch.h"
 #include "mlir/Pass/Pass.h"
 #include "mlir/Tools/Plugins/PassPlugin.h"
-#include "llvm/Support/raw_ostream.h"
 #include "llvm/ADT/StringMap.h"
+#include "llvm/Support/raw_ostream.h"
 
 using namespace mlir;
 
 namespace {
-class callcountpass_kruglova : public PassWrapper<callcountpass_kruglova, OperationPass<ModuleOp>> {
+class callcountpass_kruglova
+    : public PassWrapper<callcountpass_kruglova, OperationPass<ModuleOp>> {
 public:
-    StringRef getArgument() const final { return "func-call-counter"; }
-    StringRef getDescription() const final { return "Counts calls for func.func and adds call_count attribute"; }
+  StringRef getArgument() const final { return "func-call-counter"; }
+  StringRef getDescription() const final {
+    return "Counts calls for func.func and adds call_count attribute";
+  }
 
-    void runOnOperation() override {
-        ModuleOp module = getOperation();
-        OpBuilder builder(&getContext());
-        
-        llvm::StringMap<int> callCounts;
+  void runOnOperation() override {
+    ModuleOp module = getOperation();
+    OpBuilder builder(&getContext());
 
-        // считаем вызовы
-        module.walk([&](func::CallOp callOp) {
-            StringRef callee = callOp.getCallee();
-            callCounts[callee]++;
-        });
+    llvm::StringMap<int> callCounts;
 
-        // прикрепляем атрибуты к самим функциям
-        module.walk([&](func::FuncOp funcOp) {
-            StringRef funcName = funcOp.getName();
-            int count = callCounts.lookup(funcName);
-            funcOp->setAttr("call_count", builder.getI32IntegerAttr(count));
-        });
+    // считаем вызовы
+    module.walk([&](func::CallOp callOp) {
+      StringRef callee = callOp.getCallee();
+      callCounts[callee]++;
+    });
 
-        auto totalOps = 0;
-        module.walk([&](Operation *op) { ++totalOps; });
-        llvm::outs() << "Count operations: " << totalOps << '\n';
-    }
+    // прикрепляем атрибуты к самим функциям
+    module.walk([&](func::FuncOp funcOp) {
+      StringRef funcName = funcOp.getName();
+      int count = callCounts.lookup(funcName);
+      funcOp->setAttr("call_count", builder.getI32IntegerAttr(count));
+    });
+
+    auto totalOps = 0;
+    module.walk([&](Operation *op) { ++totalOps; });
+    llvm::outs() << "Count operations: " << totalOps << '\n';
+  }
 };
 } // namespace
 
@@ -45,10 +48,11 @@ MLIR_DECLARE_EXPLICIT_TYPE_ID(callcountpass_kruglova)
 MLIR_DEFINE_EXPLICIT_TYPE_ID(callcountpass_kruglova)
 
 mlir::PassPluginLibraryInfo getFunctionCallCounterPassPluginInfo() {
-    return {MLIR_PLUGIN_API_VERSION, "callcountpass_kruglova", "1.0",
-            []() { mlir::PassRegistration<callcountpass_kruglova>(); }};
+  return {MLIR_PLUGIN_API_VERSION, "callcountpass_kruglova", "1.0",
+          []() { mlir::PassRegistration<callcountpass_kruglova>(); }};
 }
 
-extern "C" LLVM_ATTRIBUTE_WEAK mlir::PassPluginLibraryInfo mlirGetPassPluginInfo() {
-    return getFunctionCallCounterPassPluginInfo();
+extern "C" LLVM_ATTRIBUTE_WEAK mlir::PassPluginLibraryInfo
+mlirGetPassPluginInfo() {
+  return getFunctionCallCounterPassPluginInfo();
 }
